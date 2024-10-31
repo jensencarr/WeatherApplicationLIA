f0df8acbcf16595f61aa30c345d863e6e92c6123
Karl Mårtensson <karlmartenssonsales@gmail.com>
2024-10-31 14:00:28 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WeatherApplicationLIA.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
